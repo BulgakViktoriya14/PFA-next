fea35600e78dc57832ffc29d890a089516d09263
Vika Bulgak <Vika.Bulgak@KruscheCompany.com>
2021-10-01 13:55:14 +0300

    modify project

--- functions/validateDates.js
+++ functions/validateDates.js
@@ -1,14 +1,25 @@
 export function validateDates(dateStart, dateEnd) {
     let startDateComponents = dateStart.split("-");
     let endDateComponents = dateEnd.split("-");
-    alert(`${startDateComponents[1]} ${startDateComponents[2]} ${startDateComponents[0]}`);
-    alert(`${endDateComponents[1]} ${endDateComponents[2]} ${endDateComponents[0]}`);
     let startDateItem = new Date(`${startDateComponents[1]} ${startDateComponents[2]} ${startDateComponents[0]}`).getTime();
     let endDateItem = new Date(`${endDateComponents[1]} ${endDateComponents[2]} ${endDateComponents[0]}`).getTime();
 
     alert(startDateItem)
     alert(endDateItem)
 
-    alert(startDateItem < endDateItem)
-    return startDateItem < endDateItem;
+    if(startDateItem == NaN) {
+        alert(convertDateForIos(startDateItem) < convertDateForIos(endDateItem));
+        return convertDateForIos(startDateItem) < convertDateForIos(endDateItem);
+    } else {
+        alert(startDateItem < endDateItem);
+        return startDateItem < endDateItem;
+    }
+
+}
+
+
+function convertDateForIos(date) {
+    let arr = date.split(/[- :]/);
+    date = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);
+    return date;
 }
