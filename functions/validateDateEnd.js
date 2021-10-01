6addd1985982ffd992d0cebb04c73deb07fc94d2
Vika Bulgak <Vika.Bulgak@KruscheCompany.com>
2021-10-01 15:04:18 +0300

    modify project

--- functions/validateDateEnd.js
+++ functions/validateDateEnd.js
@@ -2,7 +2,7 @@ import {convertDate} from "./validateDates";
 
 export function validateDateEnd(dateEnd) {
     let currentDate = new Date().getTime();
-    alert(currentDate)
+    alert(new Date())
     alert(currentDate)
     alert(currentDate > convertDate(`${dateEnd} 00:00:00`))
     return currentDate > convertDate(`${dateEnd} 00:00:00`);
