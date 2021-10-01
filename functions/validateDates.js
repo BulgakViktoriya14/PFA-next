75105a8416072c836d602f426fe0da0d90071153
Vika Bulgak <Vika.Bulgak@KruscheCompany.com>
2021-10-01 14:09:43 +0300

    modify project

--- functions/validateDates.js
+++ functions/validateDates.js
@@ -21,5 +21,6 @@ export function validateDates(dateStart, dateEnd) {
 function convertDateForIos(date) {
     let arr = date.split(/[- :]/);
     date = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]).getTime();
+    alert('date: '+date)
     return date;
 }
