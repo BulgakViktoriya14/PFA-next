32a3320e7795d6796393df2a1786cf7df111ecb6
Vika Bulgak <Vika.Bulgak@KruscheCompany.com>
2021-10-01 13:44:39 +0300

    modify project

--- functions/validateDates.js
+++ functions/validateDates.js
@@ -1,10 +1,11 @@
 export function validateDates(dateStart, dateEnd) {
     let startDateComponents = dateStart.split("-");
     let endDateComponents = dateEnd.split("-");
-    alert(startDateComponents);
-    alert(endDateComponents);
+    alert(`${startDateComponents[1]} ${startDateComponents[2]} ${startDateComponents[0]}`);
+    alert(`${endDateComponents[1]} ${endDateComponents[2]} ${endDateComponents[0]}`);
     let startDateItem = new Date(`${startDateComponents[1]} ${startDateComponents[2]} ${startDateComponents[0]}`).getTime();
     let endDateItem = new Date(`${endDateComponents[1]} ${endDateComponents[2]} ${endDateComponents[0]}`).getTime();
 
+    alert(startDateItem < endDateItem)
     return startDateItem < endDateItem;
 }
